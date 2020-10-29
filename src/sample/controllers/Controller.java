de1114af17a8eeb20f5817e7aad3e6e23745fef5
Vladimir <vova435335>
2020-10-29 20:59:06 +0300

    Создание пользовательского интерфейса

--- src/sample/controllers/Controller.java
+++ src/sample/controllers/Controller.java
@@ -1,4 +1,18 @@
 package sample.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.MenuButton;
+
 public class Controller {
+
+    @FXML
+    private MenuButton select_mb;
+    @FXML
+    private Canvas canvas;
+
+    @FXML
+    void initialize() {
+
+    }
 }
